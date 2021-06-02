6c57509f9d9be2e71d01ab20ecbdf2cdbfbbe21f
Marsel <60897982+MarselBurdo@users.noreply.github.com>
2021-06-02 11:20:10 +0300

    reloading work

--- src/components/Defects/Form/Signage.js
+++ src/components/Defects/Form/Signage.js
@@ -8,19 +8,8 @@ import {
 } from '../../../apis/catalog.api';
 import {ReactComponent as MainLogo} from '../../../imgs/logo-signage.svg';
 import '../Registry/Defects.less';
-import {LeftOutlined, RightOutlined} from '@ant-design/icons';
 
-const {
-	Form,
-	FormBody,
-	Layout,
-	Table,
-	Space,
-	Text,
-	Divider,
-	InputNumber,
-	Button,
-} = classic;
+const {Form, FormBody, Layout, Table, Space, Text, Divider} = classic;
 export const Signage = () => {
 	const [defectsCounter, setDefectsCounter] = useState({
 		detected: 0,
@@ -29,11 +18,12 @@ export const Signage = () => {
 		validInfo: 0,
 	});
 	const [tableVar, setTableVar] = useState({
-		rowHeight: 15,
-		pageSize: 15,
+		rowHeight: 30,
+		pageSize: 25,
 		countPages: 0,
 		rows: [],
 	});
+
 	useEffect(() => {
 		/** Request data count by defects */
 		apiGetDataCountByConfigName('defectsSignage')({
@@ -87,113 +77,52 @@ export const Signage = () => {
 				}))
 			)
 			.catch((err) => console.error());
+	}, []);
 
-		apiGetUnAuthFlatData('defectsSignage')({
-			data: {
-				statusIds: [
-					'1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-					'879f0adf-0d96-449e-bcee-800f81c4e58d',
-					'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-				],
-			},
-			params: {size: tableVar.countPages},
-		})
-			.then((resp) =>
-				setTableVar((state) => ({...state, rows: [...resp.data]}))
-			)
-			.catch((err) => console.error());
-	}, [tableVar.countPages]);
-
-	// useEffect(() => {
-	//     if (tableVar.countPages < defectsCounter.validInfo) {
-	//         setTimeout(() => {
-	//             return apiGetUnAuthFlatData('defectsSignage')({
-	//                 data: {
-	//                     statusIds: [
-	//                         '1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-	//                         '879f0adf-0d96-449e-bcee-800f81c4e58d',
-	//                         'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-	//                     ],
-	//                 },
-	//                 params: {size: tableVar.countPages > tableVar.pageSize ? tableVar.countPages : tableVar.pageSize},
-	//             })
-	//                 .then((resp) =>
-	//                     setTableVar((state) => {
-	//                         let remArr = resp.data.splice(0, state.countPages - state.pageSize)
-	//                         console.log(tableVar.countPages)
-	//                         console.log({remArr})
-	//                         return ({
-	//                             ...state,
-	//                             rows: [...resp.data],
-	//                             countPages: state.countPages + state.pageSize
-	//                         })
-	//
-	//                     })
-	//                 )
-	//                 .catch((err) => console.error());
-	//
-	//         }, 5000);
-	//     }
-	// }, [tableVar.countPages, defectsCounter.validInfo]);
-
-	const handleChange = (count, type) => {
-		switch (type) {
-			case 'next':
-				if (tableVar.countPages + count < defectsCounter.validInfo) {
-					apiGetUnAuthFlatData('defectsSignage')({
-						data: {
-							statusIds: [
-								'1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-								'879f0adf-0d96-449e-bcee-800f81c4e58d',
-								'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-							],
-						},
-						params: {size: tableVar.countPages + count},
-					})
-						.then((resp) =>
-							setTableVar((state) => ({
+	useEffect(() => {
+		if (tableVar.countPages < defectsCounter.validInfo) {
+			setTimeout(() => {
+				return apiGetUnAuthFlatData('defectsSignage')({
+					data: {
+						statusIds: [
+							'1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
+							'879f0adf-0d96-449e-bcee-800f81c4e58d',
+							'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
+						],
+					},
+					params: {
+						size:
+							tableVar.countPages + tableVar.pageSize <
+							defectsCounter.validInfo
+								? tableVar.countPages + tableVar.pageSize
+								: defectsCounter.validInfo,
+					},
+				})
+					.then((resp) =>
+						setTableVar((state) => {
+							let remArr = resp.data.splice(
+								0,
+								resp.data.length - state.pageSize
+							);
+							console.log({remArr});
+							return {
 								...state,
-								rows: [
-									...resp.data.splice(
-										tableVar.countPages,
-										count
-									),
-								],
-								countPages: tableVar.countPages + count,
-							}))
-						)
-						.catch((err) => console.error());
-					break;
-				} else break;
-			default:
-				if (tableVar.countPages - count > 0) {
-					apiGetUnAuthFlatData('defectsSignage')({
-						data: {
-							statusIds: [
-								'1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-								'879f0adf-0d96-449e-bcee-800f81c4e58d',
-								'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-							],
-						},
-						params: {size: tableVar.countPages - count},
-					})
-						.then((resp) =>
-							setTableVar((state) => ({
-								...state,
-								rows: [
-									...resp.data.splice(
-										resp.data.length - tableVar.countPages,
-										count
-									),
-								],
-								countPages: tableVar.countPages - count,
-							}))
-						)
-						.catch((err) => console.error());
-					break;
-				} else break;
+								rows: [...resp.data],
+								countPages:
+									state.countPages + state.pageSize <
+									defectsCounter.validInfo
+										? state.countPages + state.pageSize
+										: defectsCounter.validInfo,
+							};
+						})
+					)
+					.catch((err) => console.error());
+			}, 10000);
+		} else if (tableVar.countPages === defectsCounter.validInfo) {
+			setTableVar((state) => ({...state, rows: [], countPages: 0}));
 		}
-	};
+	}, [tableVar.countPages, tableVar.pageSize, defectsCounter.validInfo]);
+
 	return (
 		<Form>
 			<FormBody noPadding={true}>
@@ -205,38 +134,6 @@ export const Signage = () => {
 				>
 					<Space>
 						<MainLogo />
-						<Space>
-							<InputNumber
-								itemProps={{
-									name: 'pageSizeCount',
-									label: 'строк в таблице',
-								}}
-								defaultValue={tableVar.pageSize}
-								onChange={(value) => {
-									setTableVar((state) => ({
-										...state,
-										pageSize: value,
-										countPages: value,
-									}));
-								}}
-								min={0}
-								max={defectsCounter.validInfo}
-							/>
-							<InputNumber
-								itemProps={{
-									name: 'rowHeightCount',
-									label: 'высота в таблице',
-								}}
-								defaultValue={tableVar.rowHeight}
-								onChange={(value) => {
-									setTableVar((state) => ({
-										...state,
-										rowHeight: value,
-									}));
-								}}
-								min={0}
-							/>
-						</Space>
 					</Space>
 					<Space className={'defect-info mt-16'}>
 						<Text
@@ -311,23 +208,12 @@ export const Signage = () => {
 				</Space>
 				<Layout className={'signage'}>
 					<Space
-						className={'p-8'}
+						className={'pb-8 mr-16'}
 						style={{justifyContent: 'flex-end'}}
 					>
-						<Button
-							icon={<LeftOutlined />}
-							onClick={() =>
-								handleChange(tableVar.pageSize, 'last')
-							}
-						/>
 						<Text
 							label={`Дефектов ${tableVar.countPages}/${defectsCounter.validInfo}`}
-						/>
-						<Button
-							icon={<RightOutlined />}
-							onClick={() =>
-								handleChange(tableVar.pageSize, 'next')
-							}
+							className={'mr-16'}
 						/>
 					</Space>
 					<Table
@@ -340,24 +226,16 @@ export const Signage = () => {
 							key: 'dateDetectDefect',
 							order: 'desc',
 						}}
-						// infinityMode={true}
+						empty={
+							<div className={'BaseTable__overlay'}>
+								{' '}
+								<span>Обновляем данные...</span>{' '}
+							</div>
+						}
 						zebraStyle={true}
 						dispatch={{path: 'defects.defectsSignageTable.table'}}
 						customColumnProps={customColumnProps}
 						rows={tableVar.rows}
-						// requestLoadRows={({data, params}) =>
-						//     apiGetUnAuthFlatData('defectsSignage')({
-						//         data: {
-						//             ...data,
-						//             statusIds: [
-						//                 '1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-						//                 '879f0adf-0d96-449e-bcee-800f81c4e58d',
-						//                 'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-						//             ],
-						//         },
-						//         params: {...params, page: 1, size: tableVar.pageSize}
-						//     })
-						// }
 						requestLoadConfig={apiGetUnAuthConfigByName(
 							'defectsSignage'
 						)}
@@ -380,29 +258,6 @@ export const Signage = () => {
 									);
 								},
 							},
-							// {
-							//
-							//     name: 'forceReload',
-							//     path: 'rtd.defects.defectsSignageTable.reload',
-							//     onChange: ({setSubscribeProps}) => {
-							//         setSubscribeProps({
-							//             requestLoadRows: ({data, params}) =>
-							//                 apiGetUnAuthFlatData('defectsSignage')({
-							//                     data: {
-							//                         ...data,
-							//                         statusIds: [
-							//                             '1864073a-bf8d-4df2-b02d-8e5afa63c4d0',
-							//                             '879f0adf-0d96-449e-bcee-800f81c4e58d',
-							//                             'df7d1216-6eb7-4a00-93a4-940047e8b9c0',
-							//                         ],
-							//                     },
-							//                     params: {...params, size: tableVar.pageSize}
-							//                 })
-							//
-							//         })
-							//
-							//     }
-							// }
 						]}
 					/>
 				</Layout>
