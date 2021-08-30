dd740145db8c1e58f3270cb732eac6453ea34c0e
Валиуллин Марсель Рамисович <60897982+MarselBurdo@users.noreply.github.com>
2021-08-30 14:32:38 +0300

    catalog done

--- src/components/Catalog/tableProps.js
+++ src/components/Catalog/tableProps.js
@@ -1,8 +1,235 @@
-import {Checkbox, DateText} from 'rt-design';
+import {Button, Checkbox, DateText, Row, Search, Space} from 'rt-design';
 import React from 'react';
-import {FolderOutlined, ToolOutlined} from '@ant-design/icons';
+import {FolderOutlined, ReloadOutlined, ToolOutlined} from '@ant-design/icons';
 import {dateTime} from '../Base/customColumnProps';
 import {codeNormalizer} from '../Base/Functions/TextUtils';
+import {Access} from 'mobile-inspections-base-ui';
+import {
+	AddCustomObjectOnServer,
+	EditCustomObjectOnServer,
+} from '../Base/Modals/CustomObjectOnServer';
+import {
+	AddCustomGroupOnServer,
+	EditCustomGroupOnServer,
+} from '../Base/Modals/CustomGroupOnServer';
+import {DeleteOnServer} from '../Base/Modals/DeleteOnServer';
+import {changeStorePath} from '../Base/Functions/ChangeStorePath';
+import {CustomObjectView} from '../Base/Modals/CustomObjectView';
+import {CustomGroupView} from '../Base/Modals/CustomGroupView';
+import {
+	AddDefaultObjectOnServer,
+	EditDefaultObjectOnServer,
+} from '../Base/Modals/DefaultObjectOnServer';
+import {DefaultObjectView} from '../Base/Modals/DefaultObjectView';
+import {reloadFilterFields} from '../Base/Functions/ReloadField';
+
+/**
+ *
+ * @param mainWay name of server configuration
+ * @param catalogName name of server configuration
+ * @param unique phrase on Russian
+ * @returns {JSX.object}
+ * @desc Function choice table header buttons(action modals) and view modals
+ */
+
+export const TableHeader = ({mainWay, catalogName, unique}) => {
+	const configCatalogName = (catalogName) => {
+		switch (catalogName) {
+			case 'equipments':
+			case 'defectTypical':
+				return (
+					<>
+						{/** Show modals for this roles*/}
+						<Access
+							roles={[
+								'ROLE_ADMIN',
+								'ROLE_MI_ADMIN',
+								'ROLE_MI_SHIFT_SUPERVISOR',
+								'ROLE_MI_DETOURS_CREATOR',
+							]}
+						>
+							<AddCustomObjectOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+							<AddCustomGroupOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+							<EditCustomObjectOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+							<EditCustomGroupOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+							<DeleteOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+						</Access>
+						<Button
+							icon={<ReloadOutlined />}
+							type={'primary'}
+							hidden={true}
+							subscribe={[
+								/** Action search activate btn*/
+								{
+									name: 'onSearchPush',
+									path: `rtd.${changeStorePath(
+										mainWay,
+										catalogName
+									)}.events.onSearch`,
+									onChange: ({value, setSubscribeProps}) => {
+										value &&
+											setSubscribeProps &&
+											setSubscribeProps({hidden: !value});
+									},
+								},
+								/** Action reload in mainForm.table deactivate btn*/
+								{
+									name: 'onReloadPush',
+									path: `rtd.${changeStorePath(
+										mainWay,
+										catalogName
+									)}.rows`,
+									onChange: ({value, setSubscribeProps}) => {
+										/** We might thinking about ${path}.rows array length*/
+
+										value &&
+											value.length >= 4 &&
+											setSubscribeProps &&
+											setSubscribeProps({hidden: value});
+									},
+								},
+							]}
+							dispatch={{
+								path: `${changeStorePath(
+									mainWay,
+									catalogName
+								)}.events.onReload`,
+							}}
+						/>
+						<CustomObjectView
+							mainWay={mainWay}
+							catalogName={catalogName}
+							unique={unique}
+						/>
+						<CustomGroupView
+							catalogName={catalogName}
+							mainWay={mainWay}
+						/>
+					</>
+				);
+			default:
+				return (
+					<>
+						<Access
+							roles={[
+								'ROLE_ADMIN',
+								'ROLE_MI_SHIFT_SUPERVISOR',
+								'ROLE_MI_DETOURS_CREATOR',
+							]}
+						>
+							<AddDefaultObjectOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+							<EditDefaultObjectOnServer
+								mainWay={mainWay}
+								catalogName={catalogName}
+								unique={unique}
+							/>
+						</Access>
+						<Button
+							icon={<ReloadOutlined />}
+							type={'primary'}
+							hidden={true}
+							subscribe={[
+								/** Action search activate btn*/
+								{
+									name: 'onSearchPush',
+									path: `rtd.${changeStorePath(
+										mainWay,
+										catalogName
+									)}.events.onSearch`,
+									onChange: ({value, setSubscribeProps}) => {
+										value &&
+											setSubscribeProps &&
+											setSubscribeProps({hidden: !value});
+									},
+								},
+								/** Action reload in mainForm.table deactivate btn*/
+								{
+									name: 'onReloadPush',
+									path: `rtd.${changeStorePath(
+										mainWay,
+										catalogName
+									)}.rows`,
+									onChange: ({value, setSubscribeProps}) => {
+										/** We might thinking about ${path}.rows array length*/
+										/**
+										 * This subscribe now work, but not well. If DB have add more object in departments,departments, staffWorkSchedules configs, might change value.length >= 2
+										 * */
+										value &&
+											value.length >= 2 &&
+											setSubscribeProps &&
+											setSubscribeProps({hidden: value});
+									},
+								},
+							]}
+							dispatch={{
+								path: `${changeStorePath(
+									mainWay,
+									catalogName
+								)}.events.onReload`,
+							}}
+						/>
+						<DefaultObjectView
+							mainWay={mainWay}
+							catalogName={catalogName}
+							unique={unique}
+						/>
+					</>
+				);
+		}
+	};
+	return (
+		<Space style={{justifyContent: 'space-between'}} className={'mr-8'}>
+			<Row className={'p-8'}>{configCatalogName(catalogName)}</Row>
+			<Search
+				itemProps={{name: 'onSearch'}}
+				placeholder={
+					catalogName !== 'staff'
+						? 'Введите наименование'
+						: 'Введите сотрудника'
+				}
+				dispatch={{
+					path: `${changeStorePath(
+						mainWay,
+						catalogName
+					)}.events.onSearch`,
+				}}
+				subscribe={[
+					/** Reload Search value field, clear STORE*/
+					reloadFilterFields(
+						`${changeStorePath(
+							mainWay,
+							catalogName
+						)}.events.onReload`
+					),
+				]}
+			/>
+		</Space>
+	);
+};
 
 /**
  * @desc Array of table column configuration
